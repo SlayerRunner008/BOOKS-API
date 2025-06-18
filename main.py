14ce4ce4877826cf666c9686c5dca27d903d9f0e
casizzy <scr38919@lasallebajio.edu.mx>
2025-06-18 12:33:54 -0600

    Correción de rutas

--- main.py
+++ main.py
@@ -2,18 +2,12 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware  
 from config.database import engine, Base
 from routes import homeRoute, bookRoutes, authRoute
-#Para rutas de archivos estáticos
 from fastapi.staticfiles import StaticFiles
 import os
-from fastapi.responses import FileResponse
-
-app.mount("/", StaticFiles(directory="frontend", html=True), name="frontend")
-@app.get("/")
-async def root():
-    return FileResponse("frontend/index.html")
 
 app = FastAPI(title="Library API", version="0.0.1")
 
+# CORS
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"], 
@@ -22,8 +16,13 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# Crear tablas
 Base.metadata.create_all(bind=engine)
 
+# Rutas API
 app.include_router(homeRoute.router)
 app.include_router(bookRoutes.router)
 app.include_router(authRoute.router)
+
+# Archivos estáticos (frontend)
+app.mount("/frontend", StaticFiles(directory="frontend", html=True), name="frontend")
